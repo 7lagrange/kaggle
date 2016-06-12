6b4a49545b427590c8ab7b7469f5bc0d4da56538
hongjianqi <7lagrange@gmail.com>
2016-06-12 15:46:15 -0400

    add sub sampling

--- Grupo_Bimbo_Inventory_Demand/code/utils.py
+++ Grupo_Bimbo_Inventory_Demand/code/utils.py
@@ -1,6 +1,7 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import math
+import random
 
 # 
 def rmsle(y, y_pred):
@@ -26,6 +27,20 @@ def rmsle(y, y_pred):
     terms_to_sum = [(math.log(y_pred[i] + 1) - math.log(y[i] + 1)) ** 2.0 for i,pred in enumerate(y_pred)]
     error  =  (sum(terms_to_sum) * (1.0/len(y))) ** 0.5
     return error 
+
+def sub_sample(train_path='../data/train.csv',
+               test_path='../data/test.csv'):
+    random.seed(123456)
+    n_train, n_test =  74180464, 6999251  #number of rows 
+    s = 10000 #desired sample size
+
+    skip = sorted(random.sample(xrange(1,n_train),n_train-s))
+    df = pd.read_csv(train_path, skiprows=skip)
+    df.to_csv("../small_data/train.csv",index=False)
+
+    skip = sorted(random.sample(xrange(1,n_test),n_test-s))
+    df = pd.read_csv(test_path, skiprows=skip)
+    df.to_csv("../small_data/test.csv",index=False)
 
 
 def proc_prod(prod_path='../data/producto_tabla.csv'):
